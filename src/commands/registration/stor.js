b39def8b115cd89a8d29e6d071aa6902f7e27522
Nathan Davis <ndavis@reachmedianetwork.com>
2020-04-02 12:28:52 -0500

    feat: enhanced logging for STOR command

--- src/commands/registration/stor.js
+++ src/commands/registration/stor.js
@@ -9,10 +9,13 @@ module.exports = {
     const append = command.directive === 'APPE';
     const fileName = command.arg;
 
+    log.trace({ connectionId: this.id }, "starting promise chain with waitForConnection");
     return this.connector.waitForConnection()
     .tap(() => this.commandSocket.pause())
     .then(() => Promise.try(() => this.fs.write(fileName, {append, start: this.restByteCount})))
     .then((fsResponse) => {
+      log.trace({ connectionId: this.id }, "fsResponse");
+
       let {stream, clientPath} = fsResponse;
       if (!stream && !clientPath) {
         stream = fsResponse;
@@ -21,31 +24,69 @@ module.exports = {
       const serverPath = stream.path || fileName;
 
       const destroyConnection = (connection, reject) => (err) => {
+        log.trace({ connectionId: this.id, err }, "attempting to destroy connection");
+
         try {
           if (connection) {
-            if (connection.writable) connection.end();
+            if (connection.writable) {
+              log.trace({ connectionId: this.id }, "calling connection.end");
+              connection.end();
+            }
+
+            log.trace({ connectionId: this.id  }, "calling connection.destroy");
             connection.destroy(err);
           }
         } finally {
+          log.trace({ connectionId: this.id }, "calling destroy connection rejection callback");
           reject(err);
         }
       };
 
       const streamPromise = new Promise((resolve, reject) => {
-        stream.once('error', destroyConnection(this.connector.socket, reject));
-        stream.once('finish', () => resolve());
+        const destroySocketDelegate = destroyConnection(this.connector.socket, reject);
+        stream.once('error', (err) => {
+          log.trace({ connectionId: this.id, err }, "stream.once('error')");
+          destroySocketDelegate();
+        });
+        stream.once('finish', () => {
+          log.trace({ connectionId: this.id }, "stream.once('finish'); resolving stream promise");
+          resolve();
+        });
       });
 
       const socketPromise = new Promise((resolve, reject) => {
         this.connector.socket.on('data', (data) => {
-          if (this.connector.socket) this.connector.socket.pause();
+          log.trace({ connectionId: this.id, length: data.length }, "socket.on('data')");
+
+          if (this.connector.socket) {
+            this.connector.socket.pause();
+          }
           if (stream && stream.writable) {
-            stream.write(data, () => this.connector.socket && this.connector.socket.resume());
+            log.trace({ connectionId: this.id, length: data.length }, "writing data to filesystem stream");
+
+            stream.write(data, () => {
+              log.trace({ connectionId: this.id }, "write complete");
+              if(this.connector.socket)
+              {
+                log.trace({ connectionId: this.id }, "resuming socket");
+                this.connector.socket.resume();
+              }
+            });
           }
         });
+
         this.connector.socket.once('end', () => {
-          if (stream.listenerCount('close')) stream.emit('close');
-          else stream.end();
+          log.trace({ connectionId: this.id }, "socket.on('end')");
+
+          if (stream.listenerCount('close')) {
+            log.trace({ connectionId: this.id }, "calling stream.emit('close')");
+            stream.emit('close');
+          } else {
+            log.trace({ connectionId: this.id }, "calling stream.end");
+            stream.end();
+          }
+
+          log.trace({ connectionId: this.id }, "resolving socket promise");
           resolve();
         });
         this.connector.socket.once('error', destroyConnection(stream, reject));
@@ -55,13 +96,25 @@ module.exports = {
 
       return this.reply(150).then(() => {
         if(this.connector.socket) {
+          log.trace({ connectionId: this.id }, "resuming socket after 150 reply");
           return this.connector.socket.resume();
         }
       })
       .then(() => Promise.all([streamPromise, socketPromise]))
-      .tap(() => this.emit('STOR', null, serverPath))
-      .then(() => this.reply(226, clientPath))
-      .finally(() => stream.destroy && stream.destroy());
+      .tap(() => {
+        log.trace({ connectionId: this.id }, "emitting 'STOR' event");
+        this.emit('STOR', null, serverPath);
+      })
+      .then(() => {
+        log.trace({ connectionId: this.id }, "sending 226 reply");
+        this.reply(226, clientPath);
+      })
+      .finally(() => {
+        log.trace({ connectionId: this.id }, "promise chain complete; destroying stream");
+        if(stream.destroy) {
+          stream.destroy();
+        }
+      });
     })
     .catch(Promise.TimeoutError, (err) => {
       log.error(err);
